d29601215c38474e49e7f0f4bdb2d49839f4b971
nimarhmti <nimarahmati2000@gmail.com>
2023-04-13 12:33:34 +0430

    basics setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './Recipes/recipe-list/recipe-list.component';
+import { RecipeDetailsComponent } from './Recipes/recipe-details/recipe-details.component';
+import { RecipeItemComponent } from './Recipes/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoopingItemEditComponent } from './ShoppingList/shooping-item-edit/shooping-item-edit.component';
+import { ShoppingItemEditComponent } from './shopping-list/shopping-item-edit/shopping-item-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailsComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoopingItemEditComponent,
+    ShoppingItemEditComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shopping-list/shopping-item-edit/shopping-item-edit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-item-edit',
+  templateUrl: './shopping-item-edit.component.html',
+  styleUrls: ['./shopping-item-edit.component.scss']
+})
+export class ShoppingItemEditComponent {
+
+}
